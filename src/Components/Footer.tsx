0bd6672357c468dc53d36983dc26cf37063f239e
siddiqui0013 <asifm8702@gmail.com>
2024-12-31 16:45:31 +0500

    finalised

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -5,7 +5,7 @@ import { FaTelegramPlane, FaDiscord, FaTwitter } from "react-icons/fa";
 export default function Footer() {
   return (
          <div className="w-full mt-80 my:20 mx-auto relative flex items-center my-8 justify-center">
-            <div className="box z-10 bg-[#228B22] md:w-[85%] w-[95%] rounded-2xl border-[#4E342E] border-[12px] md:p-16">
+            <div className="box z-10 md:bg-[#228B22] bg-[#4E342E] md:w-[85%] w-[95%] rounded-2xl border-[#4E342E] border-[12px] md:p-16">
               <div className="flex flex-col gap-8 items-center justify-center py-16 text-center bg-[#FFF9C4] rounded-2xl border-[#4E342E] border-4">
                 <h1 className="md:text-6xl text-5xl dark">WingleWorm Community</h1>
                 <p className="text-2xl">Influence the future of Wingle worm meme!</p>
